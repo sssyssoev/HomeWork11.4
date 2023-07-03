466f6b8612b20c0203bd0f27597b38b56dc3e98a
ssyssoev <ssyssoev@mail.ru>
2023-07-03 21:08:21 +0300

    ДЗ №11 сделано с 4-го раза?????

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,114 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        System.out.println(" Попытка сдать 11-е ДЗ с ТРЕТЬЕГО раза");
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static int enterInt(String str) {
+        Scanner s = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.printf(str);
+
+        int x = s.nextInt();
+        System.out.printf("You entered  :  " + x);
+        return x;
+    }
+
+    public static void checkIfYearLeap(int year) {
+        System.out.println("    Проверяем год на високосность");
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + "  Год високосный");
+        } else {
+            System.out.println(year + "  Год невисокосный");
         }
     }
+
+    public static void task1() {
+        String yearStr = "Введите год";
+        int year = enterInt(yearStr);
+        checkIfYearLeap(year);
+
+    }
+
+    public static void checkDeviceType(int devOS, int deviceManYear) {
+
+        int currYear = LocalDate.now().getYear();
+
+        if (deviceManYear < currYear) {
+            if (devOS == 0) System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            if (devOS == 1) System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+        } else if (deviceManYear >= currYear) {
+            if (devOS == 0) System.out.println("  Установите версию приложения для IOS по ссылке");
+            if (devOS == 1) System.out.println("  Установите версию приложения для Android по ссылке");
+        }
+    }
+
+
+    public static void task2() {
+        System.out.println("  ");
+        System.out.println("  Проверяем версию приложения");
+        int clientOS = enterInt("  Введите тип устройства: 0 - iOS/ 1 - Android");
+        int deviceManYear = enterInt("Введите год производства устройства");
+        int currentYear = LocalDate.now().getYear();
+        System.out.println("Введен " + deviceManYear + " год про-ва устройстра");
+        checkDeviceType(clientOS, deviceManYear);
+    }
+
+    public static int checkTime(int deliveryDistance) {
+
+        int deliveryTime = 0;
+
+
+        System.out.println("              ");
+
+
+        if (deliveryDistance >= 20) {
+            deliveryTime = 1;
+
+        }
+
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            deliveryTime = 2;
+
+        }
+
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            deliveryTime = 3;
+
+        }
+
+
+        if (deliveryDistance > 100) {
+
+            deliveryTime = -1;
+
+        }
+
+        if (deliveryTime >= 0) {
+            System.out.println("  доставка займет: " + deliveryTime + " дней ");
+        } else {
+
+            System.out.println("  Доставка невозможна");
+        }
+
+
+        return deliveryTime;
+    }
+
+    public static void task3() {
+        int delDist = enterInt("  введите дистанцию доставки");
+        checkTime(delDist);
+    }
 }
+
